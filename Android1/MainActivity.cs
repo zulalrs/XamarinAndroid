5791efced09bf1c8b751c897da1e0dad45ef5efa
Zülal Rabia Savrum <zulal.r.s@gmail.com>
2019-03-24 11:09:42 +0300

    Add project files.

---
+++ Android1/MainActivity.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Android1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        private TextView txtMain;   // Kullanacağımız bir Textview nesnesini burada oluşturduk.
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            // Bizim yaptığımız değişiklikler
+            txtMain = FindViewById<TextView>(Resource.Id.txtMainView); // view kısmında yer alan textview nesnemizi burada kullanmak için id sinden o nesneyi yakaladık ve txtMaine atadık.
+            txtMain.TextSize = 25f; // O nesnemizin yazı boyutunu buradan değiştirdik.
+            txtMain.Text = "Selam Xamarin"; // Yazıyı da buradan değiştirdik.
+            /// ---End----
+           
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if(drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            // Bir toast mesajı oluşturduk.Parametre olarak nerede yapılacağı, mesaj ve süresini verdik.
+            Toast.MakeText(this, "Merhaba Toast", ToastLength.Long).Show();
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            if (id == Resource.Id.nav_camera)
+            {
+                // Handle the camera action
+                // Kameraya tıklandığında toast mesajı verecek
+                var toast = Toast.MakeText(this, "Kameraya tıklandı", ToastLength.Short);
+                toast.SetMargin(50f, -250f);    // Mesajın yerini ayarladık.
+                toast.Show();
+
+            }
+            else if (id == Resource.Id.nav_gallery)
+            {
+
+            }
+            else if (id == Resource.Id.nav_slideshow)
+            {
+
+            }
+            else if (id == Resource.Id.nav_manage)
+            {
+
+            }
+            else if (id == Resource.Id.nav_share)
+            {
+
+            }
+            else if (id == Resource.Id.nav_send)
+            {
+
+            }
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+    }
+}
+
---
+++ Android2/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using static Android.Views.View;
+using Android.Views;
+using Android.Util;
+
+namespace Android2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity,IOnClickListener
+    {
+        private Button btn1,btn2;    // Burada bir button nesnesi tanımladık.
+        private Switch swtch;    // Burada bir switch nesnesi tanımladık.
+       
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // View kısmında tanımladığımız button ve switch ile burada tanımladığımız button ve switch nesnelerini bağlamamız lazım bunun için de view kısımdaki nesnelerimizi idlerinde bulup burada tanımladığımız nesnelerimize atıyoruz.
+            btn1 = FindViewById<Button>(Resource.Id.button1);
+            btn2 = FindViewById<Button>(Resource.Id.button2);
+            swtch = (Switch)FindViewById(Resource.Id.switch1);
+
+            btn1.SetOnClickListener(this);
+            btn2.SetOnClickListener(this);
+
+            // Event tetikletmenin iki yontemi var 1)
+            //btn1.Click += Btn1_Click;   // Click eventini buradan tetikliyoruz.
+
+        }
+
+        // Event tetikletmenin iki yontemi var 2)
+        private int a = 0;
+        public void OnClick(View v)
+        {
+            Button btn = (Button)v;
+            if(btn.Id==Resource.Id.button1)
+            {
+                Toast.MakeText(this, "Button1 e tıklandı", ToastLength.Long).Show();
+                try
+                {
+                    int b = a / a;
+                }
+                catch (System.Exception ex)
+                {
+                    Log.Error("Bölmede hata",ex.Message);   // Console.log gibi mesaj veriyor.Bu mesajları Device Log ta  görebiliyoruz.
+                }
+            }
+            else
+            {
+                Toast.MakeText(this, "Button2 e tıklandı", ToastLength.Long).Show();
+            }
+        }
+        private void Btn1_Click(object sender, System.EventArgs e)  // Sender bu event ne ile tetiklendiyse onu tutuyor yani burada button nesnesini tutuyor.
+        {
+            Toast.MakeText(this, "Button1 e tıklandı", ToastLength.Long).Show();    // Button1 e tıklandığında bu mesaj gösterilecek.
+        }
+
+
+        // OnStart a birşey yazmak istediğimizde onu buradaki gibi override etmeliyiz.
+        /*
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+        */
+    }
+}
